ac3a11837799a1a18330cd4e801b7e6aff5794a3
Andrii Pihal <pihaland@fit.cvut.cz>
2025-06-25 22:35:18 +0200

    Init commit

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+class BlogPostsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :set_blog_post, only: %i[show edit update destroy]
+
+  def index
+    @blog_posts = BlogPost.all
+  end
+
+  def show
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+
+  def new
+    if user_signed_in?
+      @blog_post = BlogPost.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @blog_post = BlogPost.find(params[:id])
+  end
+
+  def update
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post.destroy
+    redirect_to blog_posts_path
+  end
+
+  private
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ::ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
+
+end
+
